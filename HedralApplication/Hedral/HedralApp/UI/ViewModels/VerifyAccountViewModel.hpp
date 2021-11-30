11b2a5c413d318e78d80b5a5927880120b8df9e9
Ivan Ayala <network.9961@gmail.com>
2021-11-29 20:49:39 -0600

    status code and response on verification view model

--- HedralApplication/Hedral/HedralApp/UI/ViewModels/VerifyAccountViewModel.hpp
+++ HedralApplication/Hedral/HedralApp/UI/ViewModels/VerifyAccountViewModel.hpp
@@ -13,6 +13,8 @@ namespace Hedral
             Q_OBJECT;
 
             Q_PROPERTY(QString verificationCode READ VerificationCode WRITE VerificationCode NOTIFY VerificationCodeChanged);
+            Q_PROPERTY(QByteArray response READ Response WRITE Response NOTIFY ResponseChanged);
+            Q_PROPERTY(int statusCode READ StatusCode WRITE StatusCode NOTIFY StatusCodeChanged);
 
         public:
             explicit VerifyAccountViewModel(QObject* parent = nullptr);
@@ -22,13 +24,25 @@ namespace Hedral
             QString VerificationCode() const;
             void VerificationCode(const QString& verificationCode);
 
+            [[nodiscard]]
+            QByteArray Response() const;
+            void Response(const QByteArray& response);
+
+            [[nodiscard]]
+            int StatusCode() const;
+            void StatusCode(const int& statusCode);
+
         signals:
             void VerificationCodeChanged();
+            void ResponseChanged();
+            void StatusCodeChanged();
 
         public slots:
 
         private:
             QString m_verificationCode;
+            QByteArray m_response;
+            int m_statusCode;
         };
     }
 }
